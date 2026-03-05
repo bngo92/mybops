b718680f367a92d8503bda70dbc5dda773407b36
Bryan Ngo <bngo92@gmail.com>
2026-03-04 23:09:19 -0800

    Migrate search to tailwind

--- mybops-wasm/src/search.rs
+++ mybops-wasm/src/search.rs
@@ -1,9 +1,9 @@
 use crate::{
-    base::Button,
+    base::{Button, Input, SelectWithCallback},
     bootstrap::Collapse,
     plot::{DataView, DataViewRender},
 };
-use leptos::{either::Either, html::Input, prelude::*};
+use leptos::{either::Either, html, prelude::*};
 use web_sys::KeyboardEvent;
 
 #[component]
@@ -25,10 +25,9 @@ pub fn Search(#[prop(into)] logged_in: RwSignal<bool>) -> impl IntoView {
             <a href="#" class="tw:text-lg tw:font-medium tw:text-black! tw:no-underline!">
               "Query"
             </a>
-            <div class="d-flex gap-3">
+            <div class="tw:flex tw:gap-4">
               <Button
-                class="tw:text-white tw:bg-purple-500/80"
-                prop:style="width: 112px"
+                class="tw:w-32 tw:text-white tw:bg-purple-500/80"
                 on:click=move |_| set_split_view.set(!split_view.get())
               >
                 {button_text}
@@ -43,73 +42,75 @@ pub fn Search(#[prop(into)] logged_in: RwSignal<bool>) -> impl IntoView {
           </>
         },
         view! {
-          <>
-            <div class="mb-3">
-              <Collapse collapsed=logged_in>
-                <p>
-                  "Run SQL queries to transform your data into insights.
-                  All queries should run against the \"c\" table."
-                </p>
-                <p>
-                  <strong>"Example Queries"</strong>
-                </p>
-                <p>"Get names of songs that have more tournament and match wins than losses:"</p>
-                <code>
-                  "SELECT name, user_wins, user_losses FROM item WHERE type='track' AND user_wins > user_losses"
-                </code>
-                <p>"Get names of songs ordered by your scores:"</p>
-                <code>
-                  "SELECT name, user_score FROM item WHERE type='track' ORDER BY user_score DESC"
-                </code>
-                <p>"Count how many songs were performed by each distinct group of artists:"</p>
-                <code>
-                  "SELECT artists, COUNT(1) FROM item WHERE type='track' GROUP BY artists"
-                </code>
-                <p>"Get songs performed by Troy:"</p>
-                <code>
-                  "SELECT name, artists FROM item, json_each(metadata->'artists') WHERE json_each.value='Troy'"
-                </code>
-                <p>"Get your average score for each group of artists:"</p>
-                <code>
-                  "SELECT artists, AVG(user_score) FROM item WHERE type='track' GROUP BY artists"
-                </code>
-                <p>
-                  <strong>"Fields"</strong>
-                </p>
-                <p>
-                  "The fields you can query on are listed below.
-                  Here is the list of fields that are available for all items:"
-                </p>
-                <ul>
-                  <li>"type: string - The type of item"</li>
-                  <li>"name: string - The name of the item"</li>
-                  <li>"rating: number - The rating that you gave the item"</li>
-                  <li>"user_score: number - Score computed from tournaments and matches"</li>
-                  <li>"user_wins: number - Tournament and match wins"</li>
-                  <li>"user_losses: number - Tournament and match losses"</li>
-                  <li>"hidden: boolean - The item was hidden"</li>
-                </ul>
-                <p>"There are also fields that are specific to a single item type."</p>
-                <p>
-                  <em>"Spotify Item Fields"</em>
-                </p>
-                <p>"Type is set to 'track' for Spotify items"</p>
-                <ul>
-                  <li>"album: string - The name of the album that the track appears on"</li>
-                  <li>
-                    "artists: array of string - The names of the artists who performed the track"
-                  </li>
-                  <li>"duration_ms: number - The track length in milliseconds"</li>
-                  <li>"popularity - Spotify popularity of the track"</li>
-                  <li>"track_number - The number of the track"</li>
-                </ul>
-              </Collapse>
-            </div>
+          <div class="tw:flex tw:flex-col tw:gap-4">
+            <Collapse collapsed=logged_in>
+              <p>
+                "Run SQL queries to transform your data into insights.
+                All queries should run against the \"c\" table."
+              </p>
+              <p>
+                <strong>"Example Queries"</strong>
+              </p>
+              <p>"Get names of songs that have more tournament and match wins than losses:"</p>
+              <code>
+                "SELECT name, user_wins, user_losses FROM item WHERE type='track' AND user_wins > user_losses"
+              </code>
+              <p>"Get names of songs ordered by your scores:"</p>
+              <code>
+                "SELECT name, user_score FROM item WHERE type='track' ORDER BY user_score DESC"
+              </code>
+              <p>"Count how many songs were performed by each distinct group of artists:"</p>
+              <code>"SELECT artists, COUNT(1) FROM item WHERE type='track' GROUP BY artists"</code>
+              <p>"Get songs performed by Troy:"</p>
+              <code>
+                "SELECT name, artists FROM item, json_each(metadata->'artists') WHERE json_each.value='Troy'"
+              </code>
+              <p>"Get your average score for each group of artists:"</p>
+              <code>
+                "SELECT artists, AVG(user_score) FROM item WHERE type='track' GROUP BY artists"
+              </code>
+              <p>
+                <strong>"Fields"</strong>
+              </p>
+              <p>
+                "The fields you can query on are listed below.
+                Here is the list of fields that are available for all items:"
+              </p>
+              <ul>
+                <li class="tw:list-disc">"type: string - The type of item"</li>
+                <li class="tw:list-disc">"name: string - The name of the item"</li>
+                <li class="tw:list-disc">"rating: number - The rating that you gave the item"</li>
+                <li class="tw:list-disc">
+                  "user_score: number - Score computed from tournaments and matches"
+                </li>
+                <li class="tw:list-disc">"user_wins: number - Tournament and match wins"</li>
+                <li class="tw:list-disc">"user_losses: number - Tournament and match losses"</li>
+                <li class="tw:list-disc">"hidden: boolean - The item was hidden"</li>
+              </ul>
+              <p>"There are also fields that are specific to a single item type."</p>
+              <p>
+                <em>"Spotify Item Fields"</em>
+              </p>
+              <p>"Type is set to 'track' for Spotify items"</p>
+              <ul>
+                <li class="tw:list-disc">
+                  "album: string - The name of the album that the track appears on"
+                </li>
+                <li class="tw:list-disc">
+                  "artists: array of string - The names of the artists who performed the track"
+                </li>
+                <li class="tw:list-disc">
+                  "duration_ms: number - The track length in milliseconds"
+                </li>
+                <li class="tw:list-disc">"popularity - Spotify popularity of the track"</li>
+                <li class="tw:list-disc">"track_number - The number of the track"</li>
+              </ul>
+            </Collapse>
             {move || {
               if split_view.get() {
                 Either::Left(
                   view! {
-                    <div class="d-flex gap-3">
+                    <div class="tw:flex tw:gap-4">
                       <SearchPane />
                       <SearchPane />
                     </div>
@@ -125,14 +126,14 @@ pub fn Search(#[prop(into)] logged_in: RwSignal<bool>) -> impl IntoView {
                 )
               }
             }}
-          </>
+          </div>
         },
     )
 }
 
 #[component]
 pub fn SearchPane() -> impl IntoView {
-    let search_ref = NodeRef::<Input>::new();
+    let search_ref = NodeRef::<html::Input>::new();
     let (query, set_query) = signal(None);
     let (error, set_error) = signal(None);
     let (view, set_view) = signal(DataView::Table);
@@ -160,74 +161,51 @@ pub fn SearchPane() -> impl IntoView {
         }
     });
 
-    let default_search = Some("SELECT name, user_score FROM item");
+    let default_search = "SELECT name, user_score FROM item";
     let onkeydown = |event: KeyboardEvent| {
         if event.key_code() == 13 {
             event.prevent_default();
         }
     };
-    let class = move || {
-        if error.get().is_some() {
-            "flex-grow-1 is-invalid"
-        } else {
-            "flex-grow-1"
-        }
-    };
-    let error = move || {
-        error
-            .get()
-            .map(|error| view! { <div class="invalid-feedback">{error}</div> })
-    };
     view! {
-      <div class="row w-100">
-        <div class="col-auto">
-          <select
-            class="form-select mb-3"
-            on:change:target=move |ev| {
-              set_view
-                .set(
-                  match ev.target().value().as_str() {
-                    "Table" => DataView::Table,
-                    "Column Graph" => DataView::ColumnGraph,
-                    "Line Graph" => DataView::LineGraph,
-                    "Scatter Plot" => DataView::ScatterPlot,
-                    "Cumulative Line Graph" => DataView::CumLineGraph,
-                    "CSV" => DataView::Csv,
-                    _ => unreachable!(),
-                  },
-                )
-            }
-          >
+      <div class="tw:flex tw:flex-col tw:gap-4 tw:w-full">
+        <div class="tw:w-fit">
+          <SelectWithCallback on_change=move |ev| {
+            set_view
+              .set(
+                match ev.target().value().as_str() {
+                  "Table" => DataView::Table,
+                  "Column Graph" => DataView::ColumnGraph,
+                  "Line Graph" => DataView::LineGraph,
+                  "Scatter Plot" => DataView::ScatterPlot,
+                  "Cumulative Line Graph" => DataView::CumLineGraph,
+                  "CSV" => DataView::Csv,
+                  _ => unreachable!(),
+                },
+              )
+          }>
             <option selected=true>"Table"</option>
             <option>"Column Graph"</option>
             <option>"Line Graph"</option>
             <option>"Scatter Plot"</option>
             <option>"Cumulative Line Graph"</option>
             <option>"CSV"</option>
-          </select>
+          </SelectWithCallback>
         </div>
         <form on:keydown=onkeydown>
-          <div class="d-flex gap-2">
-            <div class="flex-grow-1">
-              // Copy only the styles from .form-control that are needed for sizing
-              <input
-                node_ref=search_ref
-                type="text"
-                class=class
-                style="padding: .5rem 1rem; font-size: .875rem; border-width: 1px; min-width: 100%"
-                placeholder=default_search
+          <div class="tw:flex tw:gap-2">
+            <div class="tw:flex-1">
+              <Input
+                input_ref=search_ref
+                default=default_search
+                value=Some(String::new())
+                onclick=move |_| {
+                  search.dispatch(());
+                }
+                error=error
+                disabled=false
               />
-              {error}
             </div>
-            <Button
-              class="tw:text-white tw:bg-primary"
-              on:click=move |_| {
-                search.dispatch(());
-              }
-              prop:style="height: fit-content"
-            >
-              "Search"
-            </Button>
             <Button
               class="tw:text-white tw:bg-primary"
               on:click=move |_| {
